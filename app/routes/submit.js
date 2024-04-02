40d720caffea277c410385c7d41a08d4f2e4412d
Rana Salem <ranatasalem@gmail.com>
2024-04-02 15:04:29 +0100

    add button on home view to link to submit view + bump version (0.3.4)

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -1,14 +1,31 @@
-const { POST } = require('../constants/http-verbs')
+const { GET, POST } = require('../constants/http-verbs')
+const { USER } = require('ffc-auth/scopes')
 
 module.exports = [
+  {
+    method: GET,
+    path: '/submit',
+    options: {
+      auth: { strategy: 'jwt', scope: [USER] }
+    },
+    handler: async (_request, h) => {
+      return h.view('submit')
+    }
+  },
   {
     method: POST,
     path: '/submit',
     options: {
       auth: false
     },
-    handler: (request, h) => {
-      return h.view('submit')
+    handler: async (request, h) => {
+      try {
+        const crn = request.payload.crn
+        console.log(`CRN (Customer Reference Number): ${crn}`)
+        return h.redirect('/home')
+      } catch (error) {
+        console.error(`Failed to submit CRN: ${error}`)
+      }
     }
   }
 ]
