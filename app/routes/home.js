824e33f8d80ee9ed548e7a690dd6969de5c86e6b
Rana Salem <ranatasalem@gmail.com>
2024-04-09 12:56:05 +0100

    Trigger Service Bus message from AHWP stub (#3)

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,13 @@
 const { GET } = require('../constants/http-verbs')
 const { USER } = require('ffc-auth/scopes')
 
-module.exports = [{
-  method: GET,
-  path: '/home',
-  options: { auth: { strategy: 'jwt', scope: [USER] } },
-  handler: (_request, h) => {
-    return h.view('home')
+module.exports = [
+  {
+    method: GET,
+    path: '/home',
+    options: { auth: { strategy: 'jwt', scope: [USER] } },
+    handler: (_request, h) => {
+      return h.view('home')
+    }
   }
-}]
+]
---
+++ app/routes/submit.js
@@ -0,0 +1,63 @@
+const { MessageSender } = require('ffc-messaging')
+const { GET, POST } = require('../constants/http-verbs')
+const { USER } = require('ffc-auth/scopes')
+
+module.exports = [
+  {
+    method: GET,
+    path: '/submit',
+    options: {
+      auth: { strategy: 'jwt', scope: [USER] }
+    },
+    handler: async (_request, h) => {
+      return h.view('submit')
+    }
+  },
+  {
+    method: POST,
+    path: '/submit',
+    options: {
+      auth: { strategy: 'jwt', scope: [USER] }
+    },
+    handler: async (request, h) => {
+      try {
+        const sender = new MessageSender({
+          useCredentialChain: false,
+          host: process.env.MESSAGE_HOST,
+          username: process.env.MESSAGE_USER,
+          password: process.env.MESSAGE_PASSWORD,
+          address: process.env.PROCESSOR_TOPIC_ADDRESS
+        })
+        await sender.sendMessage({
+          body: {
+            scheme: request.payload.scheme,
+            tags: [request.payload.tags],
+            crn: request.payload.crn,
+            sbi: request.payload.sbi,
+            content: {
+              heading: request.payload.heading,
+              body: request.payload.body
+            },
+            requestedDate: request.payload.requestedDate
+          },
+          type: 'submit',
+          source: 'ffc-sfd-ahwp-stub'
+        })
+        const scheme = request.payload.scheme
+        const tags = request.payload.tags
+        const crn = request.payload.crn
+        const sbi = request.payload.sbi
+        const heading = request.payload.heading
+        const body = request.payload.body
+        const requestedDate = request.payload.requestedDate
+        console.log(
+          `MESSAGE SUBMITTED: [\nScheme: ${scheme},\nTags: ${tags},\nCRN: ${crn},\nSBI: ${sbi},\nContent: {\nHeading: ${heading},\nBody: ${body}\n},\nRequested Date: ${requestedDate}\n`
+        )
+        return h.redirect('/home')
+      } catch (error) {
+        console.error(`Failed to submit CRN to Service Bus: ${error}`)
+        return h.redirect('/submit')
+      }
+    }
+  }
+]
