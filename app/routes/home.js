8e7880790e5383b325e5ef7769ddbdb6159449ea
John Watson <john.watson1@defra.gov.uk>
2024-04-20 22:02:31 +0100

    Add SFD link

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,6 @@
 const { GET } = require('../constants/http-verbs')
 const { USER } = require('ffc-auth/scopes')
+const { serverConfig } = require('../config')
 
 module.exports = [
   {
@@ -7,7 +8,7 @@ module.exports = [
     path: '/home',
     options: { auth: { strategy: 'jwt', scope: [USER] } },
     handler: (_request, h) => {
-      return h.view('home')
+      return h.view('home', { singleFrontDoorUrl: serverConfig.singleFrontDoorUrl })
     }
   }
 ]
