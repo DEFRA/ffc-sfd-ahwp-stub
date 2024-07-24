edfa3ce55eb7674bf6ecb8f6aa5906a941ac2f67
Rana Salem <ranatasalem@gmail.com>
2024-07-24 09:38:26 +0100

    refactor message sender

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -1,4 +1,4 @@
-const { MessageSender } = require('ffc-messaging')
+const { sender } = require('../config')
 const { GET, POST } = require('../constants/http-verbs')
 const { USER } = require('../auth/scopes')
 
@@ -21,26 +21,21 @@ module.exports = [
     },
     handler: async (request, h) => {
       try {
-        const sender = new MessageSender({
-          useCredentialChain: false,
-          host: process.env.MESSAGE_HOST,
-          username: process.env.MESSAGE_USER,
-          password: process.env.MESSAGE_PASSWORD,
-          address: process.env.MESSAGES_TOPIC_ADDRESS
-        })
         await sender.sendMessage({
           body: {
             scheme: request.payload.scheme,
             tags: [request.payload.tags],
             crn: request.payload.crn,
             sbi: request.payload.sbi,
-            heading: request.payload.heading,
+            organisationId:
+              heading: request.payload.heading,
             body: request.payload.body,
             requestedDate: request.payload.requestedDate
           },
           type: 'submit',
           source: 'ffc-sfd-ahwp-stub'
         })
+
         const scheme = request.payload.scheme
         const tags = request.payload.tags
         const crn = request.payload.crn
@@ -49,8 +44,9 @@ module.exports = [
         const body = request.payload.body
         const requestedDate = request.payload.requestedDate
         console.log(
-          `MESSAGE SUBMITTED: [\nScheme: ${scheme},\nTags: ${tags},\nCRN: ${crn},\nSBI: ${sbi},\nHeading: ${heading},\nBody: ${body},\nRequested Date: ${requestedDate}\n`
+          `MESSAGE SUBMITTED: \nScheme: ${scheme},\nTags: ${tags},\nCRN: ${crn},\nSBI: ${sbi},\nHeading: ${heading},\nBody: ${body},\nRequested Date: ${requestedDate}\n`
         )
+        
         return h.redirect('/home')
       } catch (error) {
         console.error(`Failed to submit CRN to Service Bus: ${error}`)
