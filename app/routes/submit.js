3b600798ef8028e6f935dfc54d27bf4e3b26b42d
Rana Salem <ranatasalem@gmail.com>
2024-07-26 12:45:19 +0100

    381534 Spike: Email link login (#9)

--- app/routes/submit.js
+++ app/routes/submit.js
@@ -1,4 +1,4 @@
-const { MessageSender } = require('ffc-messaging')
+const { sender } = require('../config')
 const { GET, POST } = require('../constants/http-verbs')
 const { USER } = require('../auth/scopes')
 
@@ -21,19 +21,13 @@ module.exports = [
     },
     handler: async (request, h) => {
       try {
-        const sender = new MessageSender({
-          useCredentialChain: false,
-          host: process.env.MESSAGE_HOST,
-          username: process.env.MESSAGE_USER,
-          password: process.env.MESSAGE_PASSWORD,
-          address: process.env.MESSAGES_TOPIC_ADDRESS
-        })
         await sender.sendMessage({
           body: {
             scheme: request.payload.scheme,
             tags: [request.payload.tags],
             crn: request.payload.crn,
             sbi: request.payload.sbi,
+            organisationId: request.payload.organisationId,
             heading: request.payload.heading,
             body: request.payload.body,
             requestedDate: request.payload.requestedDate
@@ -41,16 +35,9 @@ module.exports = [
           type: 'submit',
           source: 'ffc-sfd-ahwp-stub'
         })
-        const scheme = request.payload.scheme
-        const tags = request.payload.tags
-        const crn = request.payload.crn
-        const sbi = request.payload.sbi
-        const heading = request.payload.heading
-        const body = request.payload.body
-        const requestedDate = request.payload.requestedDate
-        console.log(
-          `MESSAGE SUBMITTED: [\nScheme: ${scheme},\nTags: ${tags},\nCRN: ${crn},\nSBI: ${sbi},\nHeading: ${heading},\nBody: ${body},\nRequested Date: ${requestedDate}\n`
-        )
+
+        console.log('Message successfully submitted')
+
         return h.redirect('/home')
       } catch (error) {
         console.error(`Failed to submit CRN to Service Bus: ${error}`)
