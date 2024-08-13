1f7b9562f5ce9127b2160c4e8192e0e9f143c53a
JungaBunga <amir.ishmuhametov@gmail.com>
2024-08-13 16:28:20 +0100

    added full feedback on files upload

--- app/routes/files.js
+++ app/routes/files.js
@@ -20,7 +20,9 @@ module.exports = [
       payload: {
         parse: true,
         allow: 'multipart/form-data',
-        multipart: { output: 'stream' }
+        multipart: { output: 'stream' },
+        maxBytes: 150 * 1024 * 1024,
+        timeout: false
       }
     },
     handler: async (request, h) => {
@@ -31,45 +33,80 @@ module.exports = [
         return h.response({ error: 'No files uploaded' }).code(400)
       }
 
-      const form = new FormData()
-      form.append('scheme', scheme)
-      form.append('collection', collection)
-
       let uploadedFiles = []
+      console.log(files.hapi.filename)
 
       if (Array.isArray(files)) {
         files.forEach(file => {
-          const sanitizedFilename = file.hapi.filename.split('-').pop()
-          form.append('files', file._data, sanitizedFilename)
-          uploadedFiles.push({ filename: sanitizedFilename, blobReference: file.hapi.filename })
+          const sanitizedFilename = file.hapi.filename // in case we need to change format
+          uploadedFiles.push({ file, sanitizedFilename })
         })
       } else {
-        const sanitizedFilename = files.hapi.filename.split('-').pop()
-        form.append('files', files._data, sanitizedFilename)
-        uploadedFiles.push({ filename: sanitizedFilename, blobReference: files.hapi.filename })
+        const sanitizedFilename = files.hapi.filename // in case we need to change format
+        uploadedFiles.push({ file: files, sanitizedFilename })
       }
 
-      try {
-        const { res, payload: responsePayload } = await Wreck.post('http://ffc-sfd-file-processor:3019/upload', {
-          payload: form,
-          headers: form.getHeaders()
-        })
+      let fileData = []
 
-        const responseData = JSON.parse(responsePayload)
-        const fileData = responseData.map(file => ({
-          filename: file.metadata.filename,
-          blobReference: file.metadata.blobReference
-        }))
-        sessionMetadata.push(...fileData)
+      for (const { file, sanitizedFilename } of uploadedFiles) {
+        const form = new FormData()
+        form.append('scheme', scheme)
+        form.append('collection', collection)
+        form.append('files', file._data, sanitizedFilename)
 
-        return h.view('files', {
-          message: 'Files uploaded successfully',
-          files: sessionMetadata
-        })
-      } catch (err) {
-        console.error('Upload error:', err)
-        return h.response({ error: 'File upload failed', details: err.message }).code(500)
+        try {
+          const { res, payload: responsePayload } = await Wreck.post('http://ffc-sfd-file-processor:3019/upload', {
+            payload: form,
+            headers: form.getHeaders()
+          })
+
+          const responseData = JSON.parse(responsePayload)
+          responseData.forEach(file => {
+            if (file.error) {
+              console.error(`Error: ${file.error}, Filename: ${file.fileName}`)
+              fileData.push({
+                filename: file.fileName,
+                status: 'Error',
+                error: file.error
+              })
+            } else {
+              fileData.push({
+                filename: file.metadata.filename,
+                blobReference: file.metadata.blobReference,
+                scheme: file.metadata.scheme,
+                collection: file.metadata.collection,
+                status: 'Success'
+              })
+            }
+          })
+        } catch (err) {
+          let simplifiedError = ''
+          if (err.message === 'Response Error: 413 Payload Too Large') {
+            simplifiedError = 'Cannot exceed 10 MB'
+          }
+          fileData.push({
+            filename: sanitizedFilename,
+            status: 'Error',
+            error: simplifiedError
+          })
+        }
       }
+
+      sessionMetadata.push(...fileData)
+
+      const allSuccess = fileData.every(file => file.status === 'Success')
+      const allFailed = fileData.every(file => file.status === 'Error')
+      let message = 'Files uploaded successfully'
+      if (allFailed) {
+        message = 'All uploads failed'
+      } else if (!allSuccess) {
+        message = 'Partial upload successful'
+      }
+
+      return h.view('files', {
+        message,
+        files: sessionMetadata
+      })
     }
   }
 ]
